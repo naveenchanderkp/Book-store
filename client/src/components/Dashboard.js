2142db094553e2b8afedf9fc1d80681e46084cbe
naveen <naveenjo98@gmail.com>
2024-10-13 21:16:04 +0530

    register and login

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to the book shop!!</h1>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ client/src/components/Routes/routes.js
@@ -0,0 +1,12 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Signup from '../Signup'
+import Login from '../Login'
+
+
+const RoutesApp = () =>{
+
+
+    
+}
+
+export default RoutesApp
